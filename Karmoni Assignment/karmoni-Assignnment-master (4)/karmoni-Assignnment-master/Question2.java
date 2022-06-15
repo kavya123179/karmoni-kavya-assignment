37c2aca94127c55f1dbb34b6383c694a8a3c52ef
Muppa Kavya <88128089+kavya123179@users.noreply.github.com>
2022-06-15 17:25:31 +0530

    Add files via upload

---
+++ Karmoni Assignment/karmoni-Assignnment-master (4)/karmoni-Assignnment-master/Question2.java
@@ -0,0 +1,26 @@
+//let us assume two string array as a={apple,fruit,text,world} and b={text,sam,try,apple,blue}
+ //compare the both array and give the count of word same in both array.
+ public class Question2
+{
+    public static  int countMatch(String[] a, String[] b) {
+
+        int count = 0;
+        int cursor = 0;
+        for(int i=0; i<a.length; i++){
+            for(int j=0; j<b.length; j++){
+                if(a[i].equals(b[j])){
+                    count++;
+                    cursor = j;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+    
+	public static void main(String[] args) {
+	    String a[] = {"apple","fruit","text","world"} ;
+	    String b[] = {"text","sam","try","apple","blue"};
+		System.out.println(countMatch(a,b));
+	}
+}
